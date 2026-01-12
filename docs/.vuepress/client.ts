9a6cdb2b9c6d9703062885899733ce06cf7f3fce
Bolo Lai <cantonbolo@gmail.com>
2026-01-12 09:52:02 +0800

    修改配置

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,10 +1,19 @@
 import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
 import { defineClientConfig } from '@vuepress/client'
+import { h } from 'vue'
+import PageContextMenu from 'vuepress-theme-plume/features/PageContextMenu.vue'
+import { Layout } from 'vuepress-theme-plume/client'
 
 import './styles/index.css'
 
 export default defineClientConfig({
   enhance({ app }) {
     app.component('RepoCard', RepoCard)
+  },
+  layouts: {
+    Layout: h(Layout, null, {
+      // 将 PageContextMenu 添加到 doc-title-after 插槽，即文章标题的右侧
+      'doc-title-after': () => h(PageContextMenu),
+    }),
   },
 })
