a309202cd018cec5a4d1baba2a3f18626af37ee7
Bolo Lai <cantonbolo@gmail.com>
2026-01-12 10:23:27 +0800

    修改配置

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -4,7 +4,7 @@ import { h } from 'vue'
 import PageContextMenu from 'vuepress-theme-plume/features/PageContextMenu.vue'
 import { Layout } from 'vuepress-theme-plume/client'
 
-import './styles/index.css'
+import './styles/index.scss'
 
 export default defineClientConfig({
   enhance({ app }) {
